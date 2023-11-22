2cbd82ff4812e7902c14e019876d78bef5e29e96
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-11-22 21:55:49 +0100

    8. DBStorage - Place

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -11,3 +12,4 @@ class City(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', cascade='all, delete-orphan', backref='cities')
