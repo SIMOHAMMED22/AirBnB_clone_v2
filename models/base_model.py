01dba4b38a1f44f7cb6e1952590649537ab3e9c4
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-11-23 15:06:41 +0100

    pycode style

--- models/base_model.py
+++ models/base_model.py
@@ -18,18 +18,18 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
         if kwargs:
             if "updated_at" in kwargs.keys():
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['updated_at'] = datetime.strptime(
+                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
             if "created_at" in kwargs.keys():
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
             if "__class__" in kwargs.keys():
                 del kwargs['__class__']
             self.__dict__.update(kwargs)
