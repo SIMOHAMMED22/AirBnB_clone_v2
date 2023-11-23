77f1d15f9d95828a9295607bdd7933fd7d0bbea9
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-11-23 13:59:54 +0100

    first and last name can be null

--- models/user.py
+++ models/user.py
@@ -13,8 +13,8 @@ class User(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
         places = relationship('Place', backref='user',
                               cascade='all, delete-orphan')
         reviews = relationship('Review', backref='user',
