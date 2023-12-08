98a73222173752ae4f25258afdf4cbfc012ab31f
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-12-08 01:46:14 +0100

    test 1.0.1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -38,8 +38,7 @@ def do_deploy(archive_path):
         run(f"rm -rf {root}/{src}/web_static ")
         run("rm -rf /data/web_static/current")
         run(f"ln -s {root}/{src} /data/web_static/current")
-        print("New version deployed!")
-        
+
         return True
 
     except Exception:
