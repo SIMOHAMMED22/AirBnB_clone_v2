946c521cc6583d6f7134c191ed584048dfe76467
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-12-27 19:40:04 +0100

    python

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,38 +1,34 @@
 #!/usr/bin/python3
-from flask import Flask
-from werkzeug.utils import escape
-"""
-    This is a multi-line comment describing the purpose of the class.
-    It can span multiple lines and is enclosed within triple quotes.
-    """
+""" script that starts a Flask web application. """
 
+from flask import Flask
 app = Flask(__name__)
 
 
-# Route definitions
 @app.route('/', strict_slashes=False)
-def hello():
+def route_hbnb():
+    """ print Hello HBNB! """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ print HBNB """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_route(text='is_cool'):
-    text = text.replace('_', ' ')
-    return f'C {escape(text)}'
+def c_is_fun(text):
+    """ display C + a specific text """
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_route(text):
-    text = text.replace('_', ' ')
-    return f'Python {escape(text)}'
+def python_is_cool(text='is cool'):
+    """ display Python + a specific text """
+    return 'Python ' + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
-    # Running the Flask app on 0.0.0.0, port 5000
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
