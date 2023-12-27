432d21024c164ec559a8277555b0a90f9298549c
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-12-27 19:30:58 +0100

    route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    A class representing a Flask web application with specified routes.
+    """
+from flask import Flask
+from werkzeug.utils import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_text(text):
+    text = escape(text).replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
