bca595c716effdc854d7cd25f35c9648e089429e
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-12-29 19:04:05 +0100

    number

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,52 +1,50 @@
 #!/usr/bin/python3
-""" script that starts a Flask web application. """
+"""5. Number template"""
 
-from flask import Flask, render_template_string
+
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello_hbnb():
-    return 'Hello HBNB!'
+@app.route("/")
+def hello():
+    """print hello"""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb')
-def display_hbnb():
-    return 'HBNB'
+@app.route("/hbnb")
+def hbnb():
+    """Display HBNB"""
+    return "HBNB"
 
 
-@app.route('/c/<text>')
-def display_c(text):
-    text = text.replace('_', ' ')
-    return f'C {text}'
+@app.route("/c/<text>")
+def c(text):
+    """Display C followed by the value of the text"""
+    return f"C {text.replace('_', ' ')}"
 
 
-@app.route('/python/', defaults={'text': 'is_cool'})
-@app.route('/python/<path:text>')
-def display_python(text):
-    text = text.replace('_', ' ')
-    return f'Python {text}'
+@app.route("/python/", defaults={'text': 'is cool'})
+@app.route("/python/<text>")
+def py(text):
+    """Display Python followed by the value of the text"""
+    return f"Python {text.replace('_', ' ')}"
 
 
-@app.route('/number/<int:n>')
-def display_number(n):
-    if isinstance(n, int):
-        return f'{n} is a number'
+@app.route("/number/<int:n>")
+def number(n):
+    """Display “n is a number” only if n is an integer"""
+    return f"{n} is a number"
 
 
-@app.route('/number_template/<int:n>')
-def display_number_template(n):
-    if isinstance(n, int):
-        template = '<html><body><h1>Number: {{ n }}</h1></body></html>'
-        return render_template_string(template, n=n)
+@app.route("/number_template/<int:n>")
+def number_temp(n):
+    """Display a HTML page only if n is an integer"""
+    return render_template('5-number.html', number=n)
 
 
-if __name__ == '__main__':
-    app.run(
-        host='0.0.0.0',
-        port=5000,
-        debug=True,
-        threaded=True,
-        ssl_context='adhoc'
-    )
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
